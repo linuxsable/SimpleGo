9a8f5c6a3c2fadb4ba5e0171b1cb507ba23b7f87
Tyler Van Hoomissen <linuxsable@gmail.com>
2013-12-11 16:22:20 -0800

    new favicon

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ var port = process.env.PORT || 5000;
 server.listen(port);
 
 app.use(express.static(path.normalize(__dirname + '/../app/public')));
+
 app.get('/*', function(req, res) {
     res.sendfile(path.normalize(__dirname + '/../app/public/index.html'));
 });
